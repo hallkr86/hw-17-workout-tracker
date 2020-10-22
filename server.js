f1102605795de83b3991001023c37d65acbb21dc
Kesha Hall <k_renee10@yahoo.com>
2020-10-22 17:59:05 -0400

    basic server setup

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+const express = require("express");
+// const mongoose = require("mongoose");
+const app = express();
+// const logger = require("morgan");
+// const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+// //const ingredientsController = require("./controllers/ingredientsController");
+// //const pizzaController = require("./controllers/pizzaController")
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app use logger
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/pizza-parlour",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   }
+// );
+
+// const connection = mongoose.connection;
+
+// connection.on("connected", () => {
+//   console.log("Mongoose successfully connected.");
+// });
+
+// connection.on("error", (err) => {
+//   console.log("Mongoose connection error: ", err);
+// });
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+// //app.use(ingredientsController);
+// //app.use(pizzaController);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
