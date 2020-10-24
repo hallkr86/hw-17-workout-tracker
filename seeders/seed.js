f1b76c4b2dceeac1c1efe5f8a777f11089480a5f
Kesha Hall <k_renee10@yahoo.com>
2020-10-23 21:53:01 -0400

    updated route

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout-tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
